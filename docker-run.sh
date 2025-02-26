9ce84b355b770912e25045dd5c7139c43ba3b10b
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-02-26 18:12:06 +0100

    Use subfolder for udev and mount device directly

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-# Resolve the actual device that /dev/rplidar points to
-DEVICE=$(readlink -f /dev/rplidar)
-
-if [ -z "$DEVICE" ]; then
-  echo "Error: /dev/rplidar not found. Please ensure your udev rule is working."
-  exit 1
-fi
-
-echo "RPLIDAR C1 detected device: $DEVICE"
-
-# Run the container, passing the actual device
-docker run -it --rm --device="$DEVICE" sllidar_ros2:latest\
-  ros2 launch sllidar_ros2 sllidar_c1_launch.py serial_port:=$DEVICE
+docker run -it --rm \
+  --device=/dev/sensors/rplidar:/dev/sensors/rplidar \
+  sllidar_ros2:latest \
+  ros2 launch sllidar_ros2 sllidar_c1_launch.py serial_port:=/dev/sensors/rplidar

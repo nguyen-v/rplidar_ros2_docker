db8b84f72cf831f638bd7d5207012d59c67a6c05
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-02-26 18:18:18 +0100

    simplify dev name

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run -it --rm \
-  --device=/dev/sensors/rplidar:/dev/sensors/rplidar \
+  --device=/dev/sensors/rplidar \
   sllidar_ros2:latest \
   ros2 launch sllidar_ros2 sllidar_c1_launch.py serial_port:=/dev/sensors/rplidar
